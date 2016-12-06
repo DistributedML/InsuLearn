514771ef2dde7aa2287bacee47b478188cb48872
Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>
2016-12-05 21:35:40 -0800

    minor fix to the run script
    Signed-off-by: Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>

--- run_raft.sh
+++ run_raft.sh
@@ -12,5 +12,5 @@ sleep 10
 xterm -e ./clientbayes hospital1 127.0.0.1:2605 127.0.0.1:2600 testdata/x1.txt testdata/y1.txt h1 &
 xterm -e ./clientbayes hospital2 127.0.0.1:2606 127.0.0.1:2600 testdata/x2.txt testdata/y2.txt h2 &
 xterm -e ./clientbayes hospital3 127.0.0.1:2607 127.0.0.1:2600 testdata/x3.txt testdata/y3.txt h3 &
-xterm -e ./clientbayes hospital3 127.0.0.1:2608 127.0.0.1:2600 testdata/x4.txt testdata/y4.txt h4 &
-xterm -e ./clientbayes hospital3 127.0.0.1:2609 127.0.0.1:2600 testdata/x5.txt testdata/y5.txt h5 &
+xterm -e ./clientbayes hospital4 127.0.0.1:2608 127.0.0.1:2600 testdata/x4.txt testdata/y4.txt h4 &
+xterm -e ./clientbayes hospital5 127.0.0.1:2609 127.0.0.1:2600 testdata/x5.txt testdata/y5.txt h5 &
