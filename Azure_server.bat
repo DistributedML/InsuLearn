01395a8f9e1a4851f080e7207c7f63260f30cf30
Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>
2017-02-20 08:37:30 -0800

    Minor Change
    Signed-off-by: Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>

--- Azure_server.bat
+++ Azure_server.bat
@@ -2,5 +2,4 @@ rem echo off
 set PATH=C:\Program Files\Matlab\R2016b\bin\win64;C:\TDM-GCC-64\bin;%PATH%
 set DBPATH=%GOPATH%\src\github.com\4180122\distbayes
 go build -o %DBPATH%\serverbayes.exe %DBPATH%\serverMatlab\server_azure.go
-pause
 start cmd /k %DBPATH%\serverbayes.exe 127.0.0.1:12456 log
