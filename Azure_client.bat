acecdf6ffe89a168c1ee69f3d2d4eaad8f04d983
Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>
2017-02-20 10:13:06 -0800

    Minor fix of stupid mistake
    Signed-off-by: Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>

--- Azure_client.bat
+++ Azure_client.bat
@@ -4,7 +4,7 @@ set DBPATH=%GOPATH%\src\github.com\4180122\distbayes
 set SERVERADD=insulearns0.canadacentral.cloudapp.azure.com:12456
 for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:"IPv4 Address"`) do set LOCALADD=%%f:12456
 set XDATA=%DBPATH%\testdata\x1.txt
-set YDATA=%DBPARG%\testdata\y1.txt
+set YDATA=%DBPATH%\testdata\y1.txt
 
 go build -o %DBPATH%\clientbayes.exe %DBPATH%\clientMatlab\client_azure.go
 start cmd /k %DBPATH%\clientbayes.exe %COMPUTERNAME% %LOCALADD% %SERVERADD% %XDATA% %YDATA% log
