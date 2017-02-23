aed28029d44badb445b48a4532a29bc7fa641733
Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>
2017-02-22 17:26:48 -0800

    Going to Brazil!
    Signed-off-by: Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>

--- Azure_client.bat
+++ Azure_client.bat
@@ -1,7 +1,7 @@
 rem echo off
 set PATH=C:\Program Files\Matlab\R2016b\bin\win64;C:\TDM-GCC-64\bin;%PATH%
 set DBPATH=%GOPATH%\src\github.com\4180122\distbayes
-set SERVERADD=10.0.0.7:12456
+set SERVERADD=10.1.0.4:12456
 for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:"IPv4 Address"`) do set LOCALADD=%%f
 set XDATA=%DBPATH%\testdata\x1.txt
 set YDATA=%DBPATH%\testdata\y1.txt
