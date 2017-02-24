d8a07213e57c659dea31c216f90a8d4e447b26be
Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>
2017-02-23 17:04:01 -0800

    Signed-off-by: Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>

--- Azure_raftserver.bat
+++ Azure_raftserver.bat
@@ -2,5 +2,5 @@ rem echo off
 set PATH=C:\Program Files\Matlab\R2016b\bin\win64;C:\TDM-GCC-64\bin;%PATH%
 set DBPATH=%GOPATH%\src\github.com\4180122\distbayes
 go build -o %DBPATH%\serverbayes.exe %DBPATH%\serverMatlab_raft\serverM_raft.go
-%DBPATH%\serverbayes.exe :12456 %DBPATH%\raftlist.azure.txt 1 log
+%DBPATH%\serverbayes.exe :12456 %DBPATH%\raftlist-azure.txt 1 log
 pause
