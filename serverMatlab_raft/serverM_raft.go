03106f13ffd9d04cee9944304e96bd6aebb96ae8
Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>
2017-02-27 22:17:30 -0800

    Signed-off-by: Alborz A. Amir-Khalili <alborza@bisicl.ad.ece.ubc.ca>

--- serverMatlab_raft/serverM_raft.go
+++ serverMatlab_raft/serverM_raft.go
@@ -343,8 +343,10 @@ func connHandler(conn *net.TCPConn) {
 	case "test_complete":
 		// node is submitting test results, update testqueue on all replicas
 		fmt.Printf("<-- Received completed test results from %v.\n", msg.NodeName)
-		repstate := state{0, msg}
-		flag := replicate(repstate)
+		if mynode.testqueue[mynode.client[msg.NodeName]][mynode.cnumhist[msg.Id]] {
+			repstate := state{0, msg}
+			flag := replicate(repstate)
+		}
 		if flag {
 			enc.Encode(response{"OK", "Test Processed"})
 		} else {
